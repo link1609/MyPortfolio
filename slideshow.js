14b142b27645a1b3f6d7e00f0c3c1d12df74ae85
Robert Hall <robert@pop-os.localdomain>
2022-09-26 11:32:09 +0100

    remove some lines for top function

--- slideshow.js
+++ slideshow.js
@@ -1,24 +1,23 @@
-let width = screen.width;
+var width = screen.availWidth;
 let height = screen.height;
 // Get the button:
 let mybutton = document.getElementById("topButton");
 
-window.addEventListener("resize", function() {
-  if (window.matchMedia("(min-width: 800px)").matches) {
-    // When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    mybutton.style.display = "block";
-  } else {
+    
+  
+    mybutton.style.display = "block"
+  }  else {
     mybutton.style.display = "none";
   }
 }
-  } else {
-    mybutton.style.display = "none";
-  }
-})
+
+
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
