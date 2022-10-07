9fb3725c844e3ed78811f4037e1246f91f3abf77
Robert Hall <robert@pop-os.localdomain>
2022-10-07 18:55:18 +0100

    complete redesign of code page

--- slideshow.js
+++ slideshow.js
@@ -1,17 +1,22 @@
-let width = screen.width;
+var width = screen.availWidth;
+let height = screen.height;
 // Get the button:
 let mybutton = document.getElementById("topButton");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 && width > 600) {
-    mybutton.style.display = "block";
-  } else {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    
+  
+    mybutton.style.display = "block"
+  }  else {
     mybutton.style.display = "none";
   }
 }
+
+
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -19,7 +24,7 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
+//Slideshow Functions
 let plusSlideIndex = 1;
 showPhotoSlides(plusSlideIndex);
 
